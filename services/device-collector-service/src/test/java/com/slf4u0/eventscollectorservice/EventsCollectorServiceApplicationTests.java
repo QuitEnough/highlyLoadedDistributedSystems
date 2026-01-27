73988e3c3536fac7d25958f75d01cc13ee9f6215
QuitEnough <w79163@gmail.com>
2026-01-27 15:13:57 +0300

    HW3: code test

---
+++ services/device-collector-service/src/test/java/com/slf4u0/eventscollectorservice/EventsCollectorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slf4u0.eventscollectorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsCollectorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
