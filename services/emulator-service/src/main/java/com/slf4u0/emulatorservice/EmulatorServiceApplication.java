ca3f3a354923b907e69d437363716d4366223b4e
QuitEnough <w79163@gmail.com>
2026-01-27 22:23:52 +0300

    HW2: code test

--- services/emulator-service/src/main/java/com/slf4u0/emulatorservice/EmulatorServiceApplication.java
+++ services/emulator-service/src/main/java/com/slf4u0/emulatorservice/EmulatorServiceApplication.java
@@ -2,7 +2,9 @@ package com.slf4u0.emulatorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class EmulatorServiceApplication {
 
