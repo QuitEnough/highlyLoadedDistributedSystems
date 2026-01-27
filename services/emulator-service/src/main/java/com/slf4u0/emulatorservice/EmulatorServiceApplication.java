50a63a926580f89feb429f1ff1c726826abdd7a3
QuitEnough <w79163@gmail.com>
2026-01-27 22:02:06 +0300

    HW2: code test

---
+++ services/emulator-service/src/main/java/com/slf4u0/emulatorservice/EmulatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.slf4u0.emulatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmulatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmulatorServiceApplication.class, args);
+    }
+
+}
---
+++ services/emulator-service/src/test/java/com/slf4u0/emulatorservice/EmulatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slf4u0.emulatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmulatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
